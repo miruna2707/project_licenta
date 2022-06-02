d7a1541af9590fae40a424d3092173afdedf56a0
miruna2707 <61750718+miruna2707@users.noreply.github.com>
2022-06-02 22:06:47 +0300

    v3

--- src/app/resort/resort.component.spec.ts
+++ src/app/resort/resort.component.spec.ts
@@ -3,23 +3,30 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ResortComponent } from './resort.component';
 
 describe('ResortComponent', () => {
-  let component: ResortComponent;
-  let fixture: ComponentFixture<ResortComponent>;
-
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ResortComponent ]
-    })
-      .compileComponents();
+      declarations: [
+        ResortComponent
+      ],
+    }).compileComponents();
   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ResortComponent);
-    component = fixture.componentInstance;
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(ResortComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'google-maps'`, () => {
+    const fixture = TestBed.createComponent(ResortComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('google-maps');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(ResortComponent);
     fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('google-maps app is running!');
   });
 });
--- src/app/resort/resort.component.ts
+++ src/app/resort/resort.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import {Loader} from "@googlemaps/js-api-loader";
+import {styles} from "./mapstyles";
+
 
 @Component({
   selector: 'app-resort',
   templateUrl: './resort.component.html',
   styleUrls: ['./resort.component.css']
 })
+
 export class ResortComponent implements OnInit {
 
-  constructor() { }
+  title = 'google-maps';
+
+  private map: google.maps.Map | undefined
 
   ngOnInit(): void {
-  }
+    let loader = new Loader({
+      apiKey: 'AIzaSyAZwy5v-ofr46H_IAJWr5tdaYb80omd8UU'
+    })
 
+    loader.load().then(() => {
+      console.log('loaded gmaps')
+
+      const location = { lat: 45.6793197320134, lng: 	23.938487153966392}
+
+        this.map = new google.maps.Map(document.getElementById("map")!, {
+
+        center: location,
+        zoom: 6,
+        styles: styles
+      })
+
+      const marker = new google.maps.Marker({
+        position: location,
+        map: this.map,
+      });
+    })
+  }
 }
